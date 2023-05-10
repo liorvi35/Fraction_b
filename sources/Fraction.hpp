10c46aeaf2afffc2877f1c164923e363ee5acc49
Lior Vinman <liorvi35@gmail.com>
2023-05-10 20:21:23 +0300

    Update Fraction.hpp

--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -1,3 +1,10 @@
+/**
+ * @brief this file contatins the declarations for Fraction class
+ * @since 10/05/2023
+ * @author Lior Vinman
+*/
+
+
 #ifndef FRACTION_HPP
 #define FRACTION_HPP
 
@@ -6,74 +13,358 @@
 
 using namespace std;
 
+
+/**
+ * @brief this constant represents the maximum of int
+*/
 #define POS_INF (int)(std::numeric_limits<int>::max())
+
+
+/**
+ * @brief this constant represents the minimum of int
+*/
 #define NEG_INF (int)(std::numeric_limits<int>::min())
 
 namespace ariel
 {
+    /**
+     * @brief this class represents a fraction as n/m with many operators
+    */
     class Fraction
     {
-    private:
-        int _numerator;
-        int _denominator;
-        void _reduce();
+        private:
 
-    public:
-        Fraction();
-        Fraction(int, int);
-        Fraction(float);
+            /**
+             * @brief this data member is the fraction's numerator
+            */
+            int _numerator;
 
-        int getNumerator();
-        int getDenominator();
+            /**
+             * @brief this data member is the fraction's denominator
+            */
+            int _denominator;
 
-        Fraction operator+(const Fraction &);
-        Fraction operator+(float);
-        friend Fraction operator+(float, const Fraction &);
+            /**
+             * @brief this function is the fraction's reduce
+            */
+            void _reduce();
 
-        Fraction operator-(const Fraction &);
-        Fraction operator-(float);
-        friend Fraction operator-(float, const Fraction &);
+        public:
 
-        Fraction operator*(const Fraction &);
-        Fraction operator*(float);
-        friend Fraction operator*(float, const Fraction &);
+            /**
+             * @brief this is the default ctor, for making 0/1 default value
+            */
+            Fraction();
 
-        Fraction operator/(const Fraction &);
-        Fraction operator/(float);
-        friend Fraction operator/(float, const Fraction &);
+            /**
+             * @brief this is standart ctor, for making fraction from two integers
+             * @param int numerator
+             * @param int denominator
+            */
+            Fraction(int, int);
 
-        Fraction &operator++();
-        Fraction operator++(int);
+            /**
+             * @brief this is converting ctor, for making a fraction from a float
+             * @param float number to convert
+             * @note float must be maximum 3 numbers after the decimal point!
+            */
+            Fraction(float);
 
-        Fraction &operator--();
-        Fraction operator--(int);
+            /**
+             * @brief this is a standard getter for numerator field
+             * @return fraction's numerator
+            */
+            int getNumerator();
 
-        bool operator==(const Fraction &) const;
-        bool operator==(float) const;
-        friend bool operator==(float, const Fraction &);
+            /**
+             * @brief this is a standard getter for denominator field
+             * @return fraction's denominator
+            */
+            int getDenominator();
 
-        bool operator!=(const Fraction &) const;
-        bool operator!=(float) const;
-        friend bool operator!=(float, const Fraction &);
+            /**
+             * @brief this is `+` operaton between two fractions
+             * @param Fraction fraction to add
+             * @return this + fraction 
+            */
+            Fraction operator+(const Fraction &);
 
-        bool operator>(const Fraction &) const;
-        bool operator>(float) const;
-        friend bool operator>(float, const Fraction &);
+            /**
+             * @brief this is `+` operaton between fraction and float
+             * @param float number to add
+             * @return this + float 
+            */
+            Fraction operator+(float);
 
-        bool operator<(float) const;
-        bool operator<(const Fraction &) const;
-        friend bool operator<(float, const Fraction &);
+            /**
+             * @brief this is `+` operaton between float and a fraction
+             * @param float float to add
+             * @param Fraction fraction to add
+             * @return float + fraction 
+             * @note a friend function
+            */
+            friend Fraction operator+(float, const Fraction &);
 
-        bool operator>=(const Fraction &) const;
-        bool operator>=(float) const;
-        friend bool operator>=(float, const Fraction &);
+            /**
+             * @brief this is a `-` operator between two fractions
+             * @param Fraction fraction to remove
+             * @return this - fraction 
+            */
+            Fraction operator-(const Fraction &);
 
-        bool operator<=(const Fraction &) const;
-        bool operator<=(float) const;
-        friend bool operator<=(float, const Fraction &);
+            /**
+             * @brief this is `-` operaton between fraction and float
+             * @param float number to remove
+             * @return this - float 
+            */
+            Fraction operator-(float);
 
-        friend ostream &operator<<(ostream &, const Fraction &);
-        friend istream &operator>>(istream &, Fraction &);
+            /**
+             * @brief this is `-` operaton between float and a fraction
+             * @param float float to remove
+             * @param Fraction fraction to remove
+             * @return float - fraction 
+             * @note a friend function
+            */
+            friend Fraction operator-(float, const Fraction &);
+
+            /**
+             * @brief this is a `*` operator between two fractions
+             * @param Fraction fraction to multiply
+             * @return this * fraction 
+            */
+            Fraction operator*(const Fraction &);
+
+            /**
+             * @brief this is `*` operaton between fraction and float
+             * @param float number to multiply
+             * @return this * float 
+            */
+            Fraction operator*(float);
+
+            /**
+             * @brief this is `*` operaton between float and a fraction
+             * @param float float to multiply
+             * @param Fraction fraction to multiply
+             * @return float * fraction 
+             * @note a friend function
+            */
+            friend Fraction operator*(float, const Fraction &);
+
+            /**
+             * @brief this is a `/` operator between two fractions
+             * @param Fraction fraction to divide
+             * @return this / fraction 
+            */
+            Fraction operator/(const Fraction &);
+
+            /**
+             * @brief this is `/` operaton between fraction and float
+             * @param float number to divide
+             * @return this / float 
+            */
+            Fraction operator/(float);
+
+            /**
+             * @brief this is `/` operaton between float and a fraction
+             * @param float float to divide
+             * @param Fraction fraction to divide
+             * @return float / fraction 
+             * @note a friend function
+            */
+            friend Fraction operator/(float, const Fraction &);
+
+            /**
+             * @brief this function is a prefix `++` operator on a fraction
+             * @return ++this
+            */
+            Fraction &operator++();
+
+            /**
+             * @brief this function is a postfix `++` operator on a fraction
+             * @return this++
+            */
+            Fraction operator++(int);
+
+            /**
+             * @brief this function is a prefix `--` operator on a fraction
+             * @return --this
+            */
+            Fraction &operator--();
+
+            /**
+             * @brief this function is a postfix `--` operator on a fraction
+             * @return this--
+            */
+            Fraction operator--(int);
+
+            /**
+             * @brief this is a `==` operator between two fractions
+             * @param Fraction fraction to compare
+             * @return is this == fraction ? 
+             * @note const to not modify the currect state of this
+            */
+            bool operator==(const Fraction &) const;
+
+            /**
+             * @brief this is `==` operaton between fraction and float
+             * @param float number compare
+             * @return is this == float ?
+             * @note const to not modify the currect state of this
+            */
+            bool operator==(float) const;
+
+            /**
+             * @brief this is `==` operaton between float and a fraction
+             * @param float float to compare
+             * @param Fraction fraction to compare
+             * @return is float == fraction ? 
+             * @note a friend function
+            */
+            friend bool operator==(float, const Fraction &);
+            
+            /**
+             * @brief this is a `!=` operator between two fractions
+             * @param Fraction fraction to compare
+             * @return is this != fraction ? 
+             * @note const to not modify the currect state of this
+            */
+            bool operator!=(const Fraction &) const;
+
+            /**
+             * @brief this is `!=` operaton between fraction and float
+             * @param float number compare
+             * @return is this != float ?
+             * @note const to not modify the currect state of this
+            */
+            bool operator!=(float) const;
+
+            /**
+             * @brief this is `!=` operaton between float and a fraction
+             * @param float float to compare
+             * @param Fraction fraction to compare
+             * @return is float != fraction ? 
+             * @note a friend function
+            */
+            friend bool operator!=(float, const Fraction &);
+
+            /**
+             * @brief this is a `>` operator between two fractions
+             * @param Fraction fraction to compare
+             * @return is this > fraction ? 
+             * @note const to not modify the currect state of this
+            */
+            bool operator>(const Fraction &) const;
+
+            /**
+             * @brief this is `>` operaton between fraction and float
+             * @param float number compare
+             * @return is this > float ?
+             * @note const to not modify the currect state of this
+            */
+            bool operator>(float) const;
+
+            /**
+             * @brief this is `>` operaton between float and a fraction
+             * @param float float to compare
+             * @param Fraction fraction to compare
+             * @return is float > fraction ? 
+             * @note a friend function
+            */
+            friend bool operator>(float, const Fraction &);
+
+            /**
+             * @brief this is a `<` operator between two fractions
+             * @param Fraction fraction to compare
+             * @return is this < fraction ? 
+             * @note const to not modify the currect state of this
+            */
+            bool operator<(float) const;
+
+            /**
+             * @brief this is `<` operaton between fraction and float
+             * @param float number compare
+             * @return is this < float ?
+             * @note const to not modify the currect state of this
+            */
+            bool operator<(const Fraction &) const;
+
+            /**
+             * @brief this is `<` operaton between float and a fraction
+             * @param float float to compare
+             * @param Fraction fraction to compare
+             * @return is float < fraction ? 
+             * @note a friend function
+            */
+            friend bool operator<(float, const Fraction &);
+
+            /**
+             * @brief this is a `>=` operator between two fractions
+             * @param Fraction fraction to compare
+             * @return is this >= fraction ? 
+             * @note const to not modify the currect state of this
+            */
+            bool operator>=(const Fraction &) const;
+
+            /**
+             * @brief this is a `>=` operator between two fractions
+             * @param float float to compare
+             * @return is this >= float ? 
+             * @note const to not modify the currect state of this
+            */
+            bool operator>=(float) const;
+
+            /**
+             * @brief this is `>=` operaton between float and a fraction
+             * @param float float to compare
+             * @param Fraction fraction to compare
+             * @return is float >= fraction ? 
+             * @note a friend function
+            */
+            friend bool operator>=(float, const Fraction &);
+
+            /**
+             * @brief this is a `<=` operator between two fractions
+             * @param Fraction fraction to compare
+             * @return is this <= fraction ? 
+             * @note const to not modify the currect state of this
+            */
+            bool operator<=(const Fraction &) const;
+
+            /**
+             * @brief this is a `<=` operator between two fractions
+             * @param float float to compare
+             * @return is this <= float ? 
+             * @note const to not modify the currect state of this
+            */
+            bool operator<=(float) const;
+
+            /**
+             * @brief this is `<=` operaton between float and a fraction
+             * @param float float to compare
+             * @param Fraction fraction to compare
+             * @return is float <= fraction ? 
+             * @note a friend function
+            */
+            friend bool operator<=(float, const Fraction &);
+
+            /**
+             * @brief this function is `<<` operator to print a fraction
+             * @param ostrean a output stream object
+             * @param Fraction a fraction to print
+             * @note print is `_numerator / _denominator`
+             * @note a friend function
+            */
+            friend ostream &operator<<(ostream &, const Fraction &);
+
+            /**
+             * @brief this function is `>>` operator to take input to fraction
+             * @param istrea a input stream object
+             * @param Fraction a fraction to print
+             * @note takes input in `_numerator<space>_denominator`
+             * @note a friend function
+            */
+            friend istream &operator>>(istream &, Fraction &);
     };
 }
 #endif
